76047438c7e652121ae332d3ae29e5ab29494bf0
edde <edde@mail.ru>
2016-07-26 15:53:20 +0300

    перед отладкой

--- app.js
+++ app.js
@@ -14,6 +14,56 @@ var urlutils = require('url');
 
 app.get('/', function(req, res){
     res.render ('translator',{
-        title: 'Заполни форму'
+        title: 'Заполни форму, падла!'
     });
 });
+
+
+
+
+app.post('/', function(req, res){
+    if (!req.body.text || req.body.text == "" ){
+        res.render('translator',{
+            title: "Веедите слово для перевода"
+        });
+    }else{
+        var url = urlutils.format({
+            protokol: 'https',
+            hostname: 'translate.yandex.ru',
+            pathname: 'api/v1.5/tr.json/translate',
+            query: {
+                key: 'trnsl.1.1.20160726T120412Z.9c0dec7207e72365.39e8fcdd3ab465305de98677ebf6b3481a7b0da6',
+                lang: req.body.lang,
+                text: req.body.text
+            }
+        });
+
+        request.get({url: url, json: true}, 
+            function(error, response, json){
+                var data = {};
+                
+                if (error || json.code != 200 ){
+                    data = {
+                        title: "ошибка при переводе слова " + req.body.text,
+                        error: json.message
+                    }
+                }else{
+                    data = {
+                        title:"перевод слова " + req.body.text + " произведен!"
+                    }
+                }
+                render('translator', data);
+            });
+
+
+
+
+
+
+
+
+
+    }
+})
+
+
